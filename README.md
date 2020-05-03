e50db8c21f32cfcff1d4729f2dafbc85fb6e5d00
Ilya Arkhipov <iliarkhpv@gmail.com>
2020-05-03 15:21:48 +0300

    add requirements, fully load new fresh version of script on board, requirements added

--- README.md
+++ README.md
@@ -5,9 +5,15 @@ Python3 should be already installed. Then use pip (or pip3, if there is a confli
 ```bash
 $ pip install -r requirements.txt
 ```
-Run insta-contest.py for getting winners.
+Create .env file in local folder and put there your instagram account login and password.
+```text
+INSTAGRAM_LOGIN=insta_login
+INSTAGRAM_PASSWORD=insta_password
+```
+Run insta-contest.py for getting winners with 'contest url' and 'username' parameters:
 ```bash
-$ python3 insta-contest.py
+$ python3 insta-contest.py https://www.instagram.com/p/B_nnz3gg7wj/ djakam_style
+Contest winners: 
 ```
 
 
--- insta-contest.py
+++ insta-contest.py
@@ -1,13 +1,15 @@
 from instabot import Bot
 import re
 import argparse
-
-bot = Bot()
-bot.login(username="cosmo_vibes2020", password="Zaq12wsx12345", ask_for_code=True)
-user_id = bot.get_user_id_from_username("lego")
-user_info = bot.get_user_info(user_id)
+from dotenv import load_dotenv
+import os
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', help='Enter url of instagram post')
+    parser.add_argument('nickname', help='Enter nickname of instagram account')
+    return parser.parse_args()
 
 
 def get_media_id_from_url(insta_url):
@@ -38,29 +40,49 @@ def get_liked_users(id_media):
     return bot.get_media_likers(id_media)
 
 
-url = 'https://www.instagram.com/p/B_nnz3gg7wj/'
-media_id = get_media_id_from_url(url)
-comments = get_media_comments(media_id)
-print(comments)
-successful_commentators = []
-liked_users = set(get_liked_users(media_id))
-
-for comment in comments:
-    mentioned_friends = get_mentioned_friends(comment)
-    for user in mentioned_friends:
-        if is_user_exist(user):
-            print('ok')
-            successful_commentators.append((comment['user']['pk'], comment['user']['username']))
-            break
-print(len(successful_commentators))
-for commentator in successful_commentators:
-    if commentator[0] not in liked_users:
-        successful_commentators.remove(commentator)
-print(len(successful_commentators))
+def get_user_followers(id_user):
+    followers = bot.get_user_followers(id_user)
+    return followers
 
 
-'''4110709400 1.khadzhimuradovich
-21470092919 iskhakov.13
-25471586854 magomedov___80
-4161004016 1.abdulaevich
-5348505961 1_ahmedovich'''
+def create_winners_set(contest_winners):
+    winners = []
+    for winner in contest_winners:
+        winners.append(winner[1])
+    return set(winners)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    username = os.getenv('INSTAGRAM_LOGIN')
+    password = os.getenv('INSTAGRAM_PASSWORD')
+    bot = Bot()
+    bot.login(username=username, password=password, ask_for_code=True)
+    user_nickname = parse_args().nickname
+    post_url = parse_args().url
+
+    user_id = bot.get_user_id_from_username(user_nickname)
+    media_id = get_media_id_from_url(post_url)
+    comments = get_media_comments(media_id)
+
+    successful_commentators = []
+    for comment in comments:
+        mentioned_friends = get_mentioned_friends(comment)
+        for user in mentioned_friends:
+            if is_user_exist(user):
+                successful_commentators.append((comment['user']['pk'], comment['user']['username']))
+                break
+    print(len(successful_commentators))
+
+    liked_users = set(get_liked_users(media_id))
+    for commentator in successful_commentators:
+        if commentator[0] not in liked_users:
+            successful_commentators.remove(commentator)
+
+    user_followers = set(get_user_followers(user_id))
+    for commentator in successful_commentators:
+        if commentator[0] not in user_followers:
+            successful_commentators.remove(commentator)
+
+    print('Winners of the instagram contest: ')
+    print(*create_winners_set(successful_commentators), sep=',')
